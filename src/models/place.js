8343d108bb4188c010332decee8ce3beff43c41f
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-07 13:13:54 +0530

    second commit

---
+++ src/models/place.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+require('../db/connection');
+
+const placeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+});
+
+const Place = mongoose.model("Place", placeSchema);
+
+module.exports = Place;
+
