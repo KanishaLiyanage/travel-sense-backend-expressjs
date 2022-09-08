b63afb2c04cafaf5da69116a863bfa65510d56ca
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-08 21:12:22 +0530

    db changes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const localURL = "mongodb://127.0.0.1:27017/travelAroundDB";
+const localURL = "mongodb://127.0.0.1:27017/travelSenseDB";
 
 mongoose.connect(process.env.ATLAS_URL);
